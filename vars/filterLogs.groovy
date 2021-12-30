38756888c9e88362244d65745ace29017439054c
vinayprasadgit <87464583+vinayprasadgit@users.noreply.github.com>
2021-12-30 14:36:24 +0530

    Update filterLogs.groovy

--- vars/filterLogs.groovy
+++ vars/filterLogs.groovy
@@ -1,1 +1,10 @@
+#!/usr/bin/env groovy
+import org.apache.commons.lang.StringUtils
 
+def call(String filter_string, int occurence){
+  def logs = currentBuild.rawBuild.getLog(1000).join('\n')
+  int count = StringUtils.countMatches(logs, filter_string);
+  if(count > occurence -1){
+    currentBuild.result='UNSTABLE'
+  }
+}
